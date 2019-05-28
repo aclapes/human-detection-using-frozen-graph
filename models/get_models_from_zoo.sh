cef718e007a2e84f2df753fa3f0ddc085a3002ef
aclapes <aclapes@cudahpc08.cvc.uab.es>
2019-05-28 19:06:01 +0200

    Copy-paste error in  get_models_from_zoo.sh

--- models/get_models_from_zoo.sh
+++ models/get_models_from_zoo.sh
@@ -7,5 +7,5 @@ tar xvzf faster_rcnn_nas_coco_2018_01_28.tar.gz
 tar xvzf faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz
 
 rm faster_rcnn_nas_coco_2018_01_28.tar.gz
-rm xvzf faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz
+rm faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz
 
